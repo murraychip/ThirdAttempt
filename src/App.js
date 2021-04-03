fa6e59ebc4f4e40334bc0642fd8bfbdf05e9be24
Chip <murray.chip@gmail.com>
2021-04-03 15:48:30 -0500

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import Main from './Main.js'
-import './App.css'; 
+import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
       <div className="App">
+          <AmplifySignOut />
           <Main />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
